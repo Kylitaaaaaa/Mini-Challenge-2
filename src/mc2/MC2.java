3caad856e8ba27570c8b6bf14d55595422c0451b
Neilson Keith P. Gardose <neilsongardose@gmail.com>
2016-02-20 18:53:36 +0800

    Merge remote-tracking branch 'origin/master'
    * origin/master:
      Update MC2.java
      edite4d some more
    
    # Conflicts:
    #	src/mc2/MC2.java

--- src/mc2/MC2.java
+++ src/mc2/MC2.java
@@ -3,5 +3,11 @@ public class MC2 {
     public static void main(String[] args) {
         Gui gui = new Gui();
     }
+<<<<<<< HEAD
+    //sex lord - neil to
+=======
     //test
+    //sex
+    //migoedit
+>>>>>>> origin/master
 }
